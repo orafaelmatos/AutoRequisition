113239e0bd1baeef20bb79e16df02f88cf801c48
Rafael Matos <130413079+orafaelmatos@users.noreply.github.com>
2023-09-05 17:55:45 -0300

    first commit

---
+++ req_compras.py
@@ -0,0 +1,7 @@
+from funcoes_hpro import hotkeys, abrir_hpro, cadastrar_usuario_senha_apertar_enter, requisitar_item_compra
+
+abrir_hpro()
+
+cadastrar_usuario_senha_apertar_enter()
+
+requisitar_item_compra()
---
+++ req_estoque.py
@@ -0,0 +1,7 @@
+from funcoes_hpro import abrir_hpro, cadastrar_usuario_senha_apertar_enter, requisitar_item_estoque
+
+abrir_hpro()
+
+cadastrar_usuario_senha_apertar_enter()
+
+requisitar_item_estoque()
